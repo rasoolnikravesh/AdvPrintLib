6f5a8e485559bef145974ad4f2e5d0dcb2b98973
shahs <shahs@DESKTOP-JU74N3P>
2020-10-17 13:00:06 +0330

    Add Project 'Console Core' With Test

---
+++ Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using AdvPrintLib;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdvPrint.PrintLn("ehsan", AdvColor.DarkGray, AdvColor.DarkMagenta);
+        }
+    }
+}
