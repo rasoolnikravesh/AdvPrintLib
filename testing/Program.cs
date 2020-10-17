0c8307b15cb5194b41062269ddfbee02f9310820
rasoolnikravesh <nikravesh.rasool@gmail.com>
2020-10-17 13:35:19 +0330

    test

---
+++ testing/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdvPrintLib;
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdvPrint.PrintLn("hello", AdvColor.Red, AdvColor.Blue); 
+        }
+    }
+}
